8bf8bfb55e94ed856655c7dc99de9250bfe6d2a6
Alex-MM23 <104031928+Alex-MM23@users.noreply.github.com>
2023-09-15 10:25:01 +0200

    15 Septiembre

--- Contenidos/Introduccion JS/main.js
+++ Contenidos/Introduccion JS/main.js
@@ -2,9 +2,58 @@ alert("Se va a cambiar el texto del parrafo")
 document.getElementById("content").innerHTML = "Nuevo Texto";
 document.getElementById("div").innerHTML = "Nuevo contenido";
 
+let array = [1,4,"hola",true];
 
 function cambiarTexto(){
 
     document.getElementById("content").innerHTML = "Nuevo Texto";
     document.getElementById("div").innerHTML = "Nuevo contenido";
+}
+
+// Ejemplo Profe
+
+let contador = 0;
+function incrementar(){
+    let parrafo = document.getElementById("p");
+
+    contador++;
+    parrafo.innerHTML = contador;
+}
+
+// bucles
+
+for (let i=1; i <10; i++){
+    console.log(i);
+}
+
+function add(){
+    let parrafo = document.getElementById("lista");
+    let input = document.getElementById("entrada");
+
+    parrafo.innerHTML = input.value + parrafo.innerHTML;
+}
+
+// Añadir elementos a array
+
+array.push(5);
+
+// Array
+
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
+
+// Ejercicio
+
+let contenido = [];
+
+function agregar(){
+    
+    let value = document.getElementById("input-array").value;
+    contenido.push(value);
+
+    for (let i = 0; i < contenido.length; i++) {
+        console.log(contenido[i]);
+    }
+
 }
