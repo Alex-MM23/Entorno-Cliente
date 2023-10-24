9358c4f1e56dd8b15ee291864855241bba2e7e2c
AlexAttomo <104031928+Alex-MM23@users.noreply.github.com>
2023-10-24 10:29:10 +0200

    24 Octubre

---
+++ Contenidos/Clase 8/main.js
@@ -0,0 +1,20 @@
+const cua1 = document.getElementById("cuadrado1");
+const cua2 = document.getElementById("cuadrado2");
+const cua3 = document.getElementById("cuadrado3");
+const content = document.getElementById("container");
+
+cua1.addEventListener("click", function(){
+    function AgregarI() {
+        let clon = cua1.cloneNode(true);
+        
+    }
+    
+    function AgregarD() {
+    
+    }
+    
+    function Eliminar() {
+    
+    }
+})
+
